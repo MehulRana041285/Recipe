2cafb1391e51bc3ba5990861e69637be68b326f8
Kiran K <k.kirank009@gmail.com>
2018-01-16 18:43:06 +0530

    first commit

---
+++ src/actions/api.js
@@ -0,0 +1,5 @@
+export const BASE_URL = '';
+export const CATEGORIES = BASE_URL.concat('categories');
+export const ITEM_LIST = BASE_URL.concat('recipes/category/');
+export const SINGLE_ITEM = BASE_URL.concat('recipes/');
+export const CONTENT = BASE_URL.concat('pages/');
